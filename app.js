fe7dcc63dfbe58e3b5c1c968dda9bb596320e327
Benedek Sandor <benedekgordonsandor@gmail.com>
2022-12-26 11:46:25 -0800

    media queries are the only thing left

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-function Site() {
-    //for the contact
-    
-
+function Website() {
     //for the theme switch
     const themeBtn = document.querySelector('.theme-btn');
     themeBtn.addEventListener('click', () =>{
         let element = document.body;
         element.classList.toggle('switch-theme');
     })
+
+    //for clearing the form after submission
+    document.querySelector('form').onsubmit = e => {
+        e.target.submit();
+        e.target.reset();
+        return false;
+     };
 }
 
-Site()
+Website()
